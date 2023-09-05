1ef935a876b7d7bdd49304c55bd88705b13a29c8
John <antarcticdepths@gmail.com>
2023-09-05 20:06:11 +0200

    D:/Git/register errors, cookie, jwt

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -5,32 +5,56 @@ const bcrypt = require("bcryptjs");
 const authMiddleware = require("../middleware/auth");
 
 router.post("/register", async (req, res) => {
-  const { username, email, password } = req.body;
+  try {
+    const { username, email, password } = req.body;
 
-  // Check if user already exists
-  const existingUser = await User.findOne({ email });
-  if (existingUser) {
-    return res.status(400).json({ message: "User already exists" });
+    // Check for missing fields
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    // Check if user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    // Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create new user
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    const savedUser = await newUser.save();
+
+    // Generate JWT token
+    const payload = {
+      id: savedUser._id, // User ID from MongoDB
+      username: savedUser.username,
+    };
+
+    const token = jwt.sign(payload, jwtSecret, { expiresIn: "1h" }); // 1 hour expiry
+
+    // Send the token in HTTP-only cookie
+    const isProduction = process.env.NODE_ENV === "production";
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: isProduction, // only transfer cookie over HTTPS in production
+      sameSite: "strict", // protection against CSRF attacks
+    });
+
+    // Send success response
+    res.status(201).json({ message: "User registered successfully", token });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Server error" });
   }
-
-  // Hash the password
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
-
-  // Create new user
-  const newUser = new User({
-    username, // <-- make sure to include this
-    email,
-    password: hashedPassword,
-    // Add other fields as necessary
-  });
-
-  await newUser.save();
-
-  // Generate JWT token (we'll implement this next)
-  // ...
-
-  res.status(201).json({ message: "User registered successfully" });
 });
 
 const jwt = require("jsonwebtoken");
