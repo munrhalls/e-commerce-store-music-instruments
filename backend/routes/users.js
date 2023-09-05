9c987632c428b825254bf0282ea89d8cd8b52d6a
John <antarcticdepths@gmail.com>
2023-09-05 19:53:26 +0200

    D:/Git/register works

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcryptjs");
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 const authMiddleware = require("../middleware/auth");
 
 router.post("/register", async (req, res) => {
@@ -13,18 +13,23 @@ router.post("/register", async (req, res) => {
     return res.status(400).json({ message: "User already exists" });
   }
 
-  // Hash password
+  // Hash the password
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
 
   // Create new user
   const newUser = new User({
-    username,
+    username, // <-- make sure to include this
     email,
     password: hashedPassword,
+    // Add other fields as necessary
   });
 
   await newUser.save();
+
+  // Generate JWT token (we'll implement this next)
+  // ...
+
   res.status(201).json({ message: "User registered successfully" });
 });
 
