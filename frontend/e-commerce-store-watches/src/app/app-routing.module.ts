8a71045003aa1b949d5b4723499fb3333019aea3
John <antarcticdepths@gmail.com>
2023-09-20 08:51:09 +0200

    Module authentication

--- frontend/e-commerce-store-watches/src/app/app-routing.module.ts
+++ frontend/e-commerce-store-watches/src/app/app-routing.module.ts
@@ -1,21 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { AuthenticateComponent } from './core/authenticate/authenticate.component';
+import { LoginComponent } from './core/authenticate/login/login.component';
+import { RegisterComponent } from './core/authenticate/register/register.component';
+import { ForgotPasswordComponent } from './core/authenticate/forgot-password/forgot-password.component';
 import { StoreComponent } from './store/store.component';
 
 const routes: Routes = [
   {
-    path: 'authenticate',
+    path: '',
     component: AuthenticateComponent,
-    data: { animation: 'authenticateAnimation' },
+    children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+      { path: 'forgot-password', component: ForgotPasswordComponent },
+    ],
   },
   { path: 'store', component: StoreComponent },
 ];
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, RouterModule.forRoot(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
