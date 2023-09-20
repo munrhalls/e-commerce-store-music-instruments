04f437b8cfbef9a25693810c456aceb4eceff317
John <antarcticdepths@gmail.com>
2023-09-20 08:56:02 +0200

    Module authentication

--- frontend/e-commerce-store-watches/src/app/core/authenticate/authenticate-routing.module.ts
+++ frontend/e-commerce-store-watches/src/app/core/authenticate/authenticate-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthenticateComponent } from './authenticate.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AuthenticateComponent,
+    children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+      { path: 'forgot-password', component: ForgotPasswordComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AuthenticateRoutingModule { }
+export class AuthenticateRoutingModule {}
--- frontend/e-commerce-store-watches/src/app/core/authenticate/authenticate.module.ts
+++ frontend/e-commerce-store-watches/src/app/core/authenticate/authenticate.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AuthenticateRoutingModule } from './authenticate-routing.module';
-
+import { AuthenticateComponent } from './authenticate.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    AuthenticateRoutingModule
-  ]
+  declarations: [
+    AuthenticateComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgotPasswordComponent,
+  ],
+  imports: [CommonModule, AuthenticateRoutingModule],
 })
-export class AuthenticateModule { }
+export class AuthenticateModule {}
