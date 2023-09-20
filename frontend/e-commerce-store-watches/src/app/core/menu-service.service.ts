36e19ca71b4541bbf2e1d3666629331fee5a9eb6
John <antarcticdepths@gmail.com>
2023-09-20 19:51:11 +0200

    Use stack DSA to fix routing back and forth - ROUTING BUGFIX

--- frontend/e-commerce-store-watches/src/app/core/menu-service.service.ts
+++ frontend/e-commerce-store-watches/src/app/core/menu-service.service.ts
@@ -1,18 +1,30 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { Router, ActivatedRoute } from '@angular/router';
+import {
+  Router,
+  ActivatedRoute,
+  Event as NavigationEvent,
+  NavigationEnd,
+} from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MenuService {
   openElementName: BehaviorSubject<string> = new BehaviorSubject('');
-  private previousUrl: string = '/';
+  private urlStack: string[] = [];
 
   constructor(private router: Router, private route: ActivatedRoute) {
-    this.route.queryParams.subscribe((params) => {
-      this.previousUrl = params['previousUrl'] || '/';
-    });
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event) => {
+        if (event instanceof NavigationEnd) {
+          if (!this.urlStack.includes(event.urlAfterRedirects)) {
+            this.urlStack.push(event.urlAfterRedirects);
+          }
+        }
+      });
   }
   setOpenElementNameManually(name: string) {
     this.openElementName.next(name);
@@ -35,8 +47,16 @@ export class MenuService {
       .catch((err) => console.error('Navigation Error:', err));
   }
   closeAuthenticateURL() {
-    this.router
-      .navigate([this.previousUrl])
-      .catch((err) => console.error('Navigation Error:', err));
+    if (this.urlStack.length > 1) {
+      this.urlStack.pop(); // Remove current URL
+      const lastUrl = this.urlStack.pop() || '/'; // Get last URL
+      this.router
+        .navigate([lastUrl])
+        .catch((err) => console.error('Navigation Error:', err));
+    } else {
+      this.router
+        .navigate(['/'])
+        .catch((err) => console.error('Navigation Error:', err)); // Default route
+    }
   }
 }
