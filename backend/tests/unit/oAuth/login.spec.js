da7f905025717d931e663cbd16eafe254b3d50cf
John <antarcticdepths@gmail.com>
2023-09-28 10:23:04 +0200

    Test google oauth

--- backend/tests/unit/oAuth/login.spec.js
+++ backend/tests/unit/oAuth/login.spec.js
@@ -5,6 +5,16 @@ beforeAll(async () => {
   await initializeApp();
 });
 
+describe("OAuth Google Login", () => {
+  it("should redirect to Google login page", async () => {
+    const response = await request(app).get("/auth/google");
+    expect(response.status).toBe(302);
+    expect(response.header.location).toContain(
+      "https://accounts.google.com/o/oauth2/"
+    );
+  });
+});
+
 afterAll(async () => {
   await closeApp();
 });
