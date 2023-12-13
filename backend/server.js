4da3083092bb20dadd8a9861bd79485bdfb0b43d
Munrhalls <antarcticdepths@gmail.com>
2023-12-13 17:36:47 +0100

    TOTAL RESET

--- backend/server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-const server = http.createServer();
-
-server.on('request', (req, res) => {});
-
-server.listen(3000, 'localhost', () => {
-  console.log('Listening on 3000...');
-});
