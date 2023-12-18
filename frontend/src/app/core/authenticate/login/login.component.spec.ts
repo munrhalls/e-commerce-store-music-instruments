3a4deb4afed023e58dbfe9f5d9f4926a9033c733
Munrhalls <antarcticdepths@gmail.com>
2023-12-18 12:02:06 +0100

    Server will be GRAPHQL, throw out REST API test

--- frontend/src/app/core/authenticate/login/login.component.spec.ts
+++ frontend/src/app/core/authenticate/login/login.component.spec.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './login.component'
 import { RouterTestingModule } from '@angular/router/testing'
 import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { ReactiveFormsModule } from '@angular/forms'
+
 describe('LoginComponent', () => {
   let component: LoginComponent
   let fixture: ComponentFixture<LoginComponent>
@@ -26,12 +27,5 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy()
-  })
-
-  describe('Google sign in button', () => {
-    it('should redirect to oauth url on server, which is /auth/google', () => {
-      const expectedURL = window.location.origin + '/auth/google'
-      expect(window.location.href === expectedURL)
-    })
   })
 })
