97bb358748d6cbcb18d6c5bf23f06373f36aa03a
Munrhalls <antarcticdepths@gmail.com>
2023-12-20 13:17:00 +0100

    ts node

--- backend/server/src/server.ts
+++ backend/server/src/server.ts
@@ -2,8 +2,8 @@ import fastify from 'fastify'
 
 const server = fastify()
 
-server.get('/ping', async (request, reply) => {
-  return 'pong\n'
+server.get('/', async (request, reply) => {
+  return 'hello world'
 })
 
 server.listen({ port: 8080 }, (err, address) => {
