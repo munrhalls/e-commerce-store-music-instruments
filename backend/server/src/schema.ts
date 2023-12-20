06f7324811bf0f12916448a37fd65e18782c7364
Munrhalls <antarcticdepths@gmail.com>
2023-12-20 16:49:25 +0100

    server schema test passes

--- backend/server/src/schema.ts
+++ backend/server/src/schema.ts
@@ -1,9 +1,17 @@
-import typeDefs from 'schema.graphql'
+import typeDefs from './schema.graphql'
 import { makeExecutableSchema } from '@graphql-tools/schema'
+
+const rndProds = [
+  { name: 'some fucking product 1', price: 49 },
+  { name: 'some fucking product 2', price: 59 },
+  { name: 'some fucking product 3', price: 69 }
+]
 
 const resolvers = {
   Query: {
-    products: () => {}
+    products: () => {
+      return rndProds
+    }
   }
 }
 
