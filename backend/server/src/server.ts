a51afd2f7b70e7fde7f9c9c9dc55f1b5e9143bcf
Munrhalls <antarcticdepths@gmail.com>
2023-12-20 14:26:11 +0100

    Code server | fix all ts warnings

--- backend/server/src/server.ts
+++ backend/server/src/server.ts
@@ -1,10 +1,52 @@
 import 'graphql-import-node'
 import fastify from 'fastify'
+import {
+  getGraphQLParameters,
+  processRequest,
+  type Request,
+  sendResult,
+  shouldRenderGraphiQL,
+  renderGraphiQL
+} from 'graphql-helix'
+import { schema } from './schema'
 
 const server = fastify()
 
-server.get('/', async (request, reply) => {
-  return 'hello world'
+server.route({
+  method: ['POST', 'GET'],
+  url: '/graphql',
+  handler: async (req, reply) => {
+    const request: Request = {
+      headers: req.headers,
+      method: req.method,
+      query: req.query,
+      body: req.body
+    }
+
+    if (shouldRenderGraphiQL(request)) {
+      await reply.header('Content-Type', 'text/html')
+
+      await reply.send(
+        renderGraphiQL({
+          endpoint: '/graphql'
+        })
+      )
+
+      return
+    }
+
+    const { operationName, query, variables } = getGraphQLParameters(request)
+
+    const result = await processRequest({
+      request,
+      schema,
+      operationName,
+      query,
+      variables
+    })
+
+    await sendResult(result, reply.raw)
+  }
 })
 
 server.listen({ port: 8080 }, (err, address) => {
