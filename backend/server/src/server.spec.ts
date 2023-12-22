0e167e4583a5ecb1ca0e0dc4cff67d44b8174bea
Munrhalls <antarcticdepths@gmail.com>
2023-12-22 18:18:47 +0100

    modif for tests

---
+++ backend/server/src/server.spec.ts
@@ -0,0 +1,28 @@
+import fetch from 'node-fetch'
+
+describe('GraphQL Server Tests', () => {
+  it('should fetch data from the GraphQL endpoint', async () => {
+    const query = `
+      query {
+        product(name: "ExampleProductName") {
+          name
+          price
+        }
+      }
+    `
+
+    const response = await fetch('http://localhost:PORT/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ query })
+    })
+
+    const data = await response.json()
+
+    expect(response.status).toBe(200) // Or other relevant status code
+    expect(data).toHaveProperty('data.product.name', 'ExampleProductName')
+    // More assertions as needed
+  })
+})
