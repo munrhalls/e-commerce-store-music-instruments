c33de1cfd5f02a5f9c6c0a705dfacad51426170c
Munrhalls <antarcticdepths@gmail.com>
2023-12-31 11:17:51 +0100

    test

--- backend/server/src/server.spec.ts
+++ backend/server/src/server.spec.ts
@@ -6,7 +6,7 @@ afterAll(async () => {
   await server.close();
 });
 
-test('GraphQL Query { products { name, price } } should return status code 200', async () => {
+test('Valid Query { products { name, price } } returns 200', async () => {
   const response = await server.inject({
     method: 'POST',
     url: '/graphql',
@@ -16,4 +16,16 @@ test('GraphQL Query { products { name, price } } should return status code 200',
   });
 
   expect(response.statusCode).toEqual(200);
+});
+
+test('Not valid Query: non-existing field returns 400 ({products {name, BLABLA, price}})', async () => {
+  const response = await server.inject({
+    method: 'POST',
+    url: '/graphql',
+    payload: {
+      query: '{products {name, blabla, price}}'
+    }
+  });
+
+  expect(response.statusCode).toEqual(400);
 });
