58cf4164a4bf74315679bc17c539e58518179010
Munrhalls <antarcticdepths@gmail.com>
2024-02-02 12:49:25 +0100

    FEATURE-TAGGING-LARGE

--- parseCommitToTag.sh
+++ parseCommitToTag.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# # FORMAT for commits: <FEATURE>-<FEATURE-NAME>-<SIZE> | <SIZE> VALUES: LARGE|MEDIUM|TINY>
 # Fetch latest commit, get details. If not <FEATURE>-<FEATURE-NAME>-<SIZE> format, exit with null
 latest_commit=$(git log --pretty=format:"%s" -1)
 if [[ $latest_commit =~ ^FEATURE-.*-(LARGE|MEDIUM|TINY)$ ]]; then
@@ -24,52 +25,3 @@ fi
 
 # Output the latest tag with properly formatted version number
 echo "FEATURE-${latest_feature_name}-${major}.${minor}.${patch}"
-
-
-
-# #!/bin/bash
-
-# # FORMAT for commits: <FEATURE>-<FEATURE-NAME>-<SIZE> | <SIZE> VALUES: LARGE|MEDIUM|TINY>
-
-# # A. Get all commit messages that respect the format
-# commit_messages=$(git log --pretty=format:"%s" | grep -E '^FEATURE-.*-(LARGE|MEDIUM|TINY)$')
-
-# latest_commit="${commit_messages[0]}"
-# latest_feature_name=$(echo "$latest_commit" | cut -d '-' -f 2)
-
-# # B & C. Process each commit message
-# declare -A feature_versions
-
-# while read -r message; do
-#     feature_name=$(echo "$message" | cut -d '-' -f 2)
-#     size=$(echo "$message" | cut -d '-' -f 3)
-
-#     # Initialize feature version if it does not exist
-#     if [[ -z ${feature_versions[$feature_name]} ]]; then
-#         feature_versions[$feature_name]="1.0.0"
-#     else
-#         # Extract the current version parts
-#         IFS='.' read -r major minor patch <<< "${feature_versions[$feature_name]}"
-#         case "$size" in
-#             LARGE)
-#                 major=$((major + 1))
-#                 minor=0
-#                 patch=0
-#                 ;;
-#             MEDIUM)
-#                 minor=$((minor + 1))
-#                 patch=0
-#                 ;;
-#             TINY)
-#                 patch=$((patch + 1))
-#                 ;;
-#         esac
-#         # Update the version
-#         feature_versions[$feature_name]="${major}.${minor}.${patch}"
-#     fi
-# done <<< "$commit_messages"
-
-# # Output the tags for each feature
-# for feature in "${!feature_versions[@]}"; do
-#     echo "FEATURE-${feature}-${feature_versions[$feature]}"
-# done
