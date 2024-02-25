d5ced0aca1f48c8ceb451ecad6fd7cb9e8bf0fb6
Munrhalls <antarcticdepths@gmail.com>
2024-02-25 03:45:28 +0100

    feature-ci/cd-tiny

---
+++ .gitlab-ci_syntax_tests.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# - |
+# echo "Checking for tag.txt file..."
+# if [ ! -f "tag.txt" ] || [ ! -s "tag.txt" ]; then
+#     echo "Error: tag.txt file is missing or empty"
+#     exit 1
+# fi
+
+# - |
+# echo "Reading GENERATED_TAG from tag.txt..."
+# export GENERATED_TAG=$(cat tag.txt) || exit 1
