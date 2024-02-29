e966ae2c84c3feb76c32debd21b44e59c002a1ee
Munrhalls <antarcticdepths@gmail.com>
2024-02-29 07:52:49 +0100

    feature-ci/cd-tiny

--- deployValidate.sh
+++ deployValidate.sh
@@ -1,29 +1,29 @@
-    - |
-    echo "Checking for tag.txt file..."
-    if [ ! -f "tag.txt" ] || [ ! -s "tag.txt" ]; then
-        echo "Error: tag.txt file is missing or empty"
-        exit 1
-    fi
+    # - |
+    # echo "Checking for tag.txt file..."
+    # if [ ! -f "tag.txt" ] || [ ! -s "tag.txt" ]; then
+    #     echo "Error: tag.txt file is missing or empty"
+    #     exit 1
+    # fi
 
-    - |
-    echo "Reading GENERATED_TAG from tag.txt..."
-    export GENERATED_TAG=$(cat tag.txt) || exit 1
+    # - |
+    # echo "Reading GENERATED_TAG from tag.txt..."
+    # export GENERATED_TAG=$(cat tag.txt) || exit 1
 
-    - |
-        if [ -z "$GENERATED_TAG" ]; then
-        echo "Error: Generated tag is empty.";
-        exit 1;
-        else
-        echo "Tag extracted: $GENERATED_TAG";
-        fi || exit 1
-        echo "Tag extracted: $GENERATED_TAG"
+    # - |
+    #     if [ -z "$GENERATED_TAG" ]; then
+    #     echo "Error: Generated tag is empty.";
+    #     exit 1;
+    #     else
+    #     echo "Tag extracted: $GENERATED_TAG";
+    #     fi || exit 1
+    #     echo "Tag extracted: $GENERATED_TAG"
 
-    - |
-    echo "Checking if GENERATED_TAG satisfies the <feature>-<feature-name>-<size> format..."
-    if [ -z "$GENERATED_TAG" ]; then
-        echo "Commit violates <feature>-<feature-name>-<size> format required for proper docker image tagging";
-        exit 1;
-    fi
+    # - |
+    # echo "Checking if GENERATED_TAG satisfies the <feature>-<feature-name>-<size> format..."
+    # if [ -z "$GENERATED_TAG" ]; then
+    #     echo "Commit violates <feature>-<feature-name>-<size> format required for proper docker image tagging";
+    #     exit 1;
+    # fi
 
     - |
     echo "Using docker-compose to build the updated images in the repository..."
