e636ac2e6b25bff4881a831df5b8e7458c94642a
Munrhalls <antarcticdepths@gmail.com>
2024-03-23 13:29:50 +0100

    test new cd

--- pushImages.sh
+++ pushImages.sh
@@ -4,5 +4,6 @@ export IMAGE_TAG=$(git describe --tags --abbrev=0)
 echo "Pushing image with tag: $IMAGE_TAG"
 
 docker login
+docker compose -f compose-local.yaml down
 docker compose -f compose-local.yaml build
 docker compose -f compose-local.yaml push
