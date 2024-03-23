86ab20e18c7fff2f0b688898f2c7b8ba509c5f70
Munrhalls <antarcticdepths@gmail.com>
2024-03-23 12:36:45 +0100

    new pipeline script + gitlab

--- pushImages.sh
+++ pushImages.sh
@@ -4,5 +4,5 @@ export IMAGE_TAG=$(git describe --tags --abbrev=0)
 echo "Pushing image with tag: $IMAGE_TAG"
 
 docker login
-docker compose -f compose-local.yaml build --build-arg IMAGE_TAG=$IMAGE_TAG
+docker compose -f compose-local.yaml build
 docker compose -f compose-local.yaml push
