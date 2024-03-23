2cafda1e2e214a4aff4a0390b35da1c3eda604f4
Munrhalls <antarcticdepths@gmail.com>
2024-03-23 12:52:20 +0100

    ui https req

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent implements OnInit, OnDestroy {
   constructor(private readonly http: HttpClient) {}
 
   getHello(): Observable<any> {
-    const url = 'http://nginx/api/hello'
+    const url = '/api/hello'
     return this.http.get(url).pipe(
       take(1),
       catchError((error) => {
