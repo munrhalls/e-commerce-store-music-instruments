9dea0f0f72d34673a81ba60ab92b127ce17bcdfa
Munrhalls <antarcticdepths@gmail.com>
2024-04-09 03:32:30 +0200

    search

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 import { NavMenuComponent } from '../nav-menu/nav-menu.component';
-
+import { SearchComponent } from '../search/search.component';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [NgOptimizedImage, NavMenuComponent],
+  imports: [NgOptimizedImage, NavMenuComponent, SearchComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
---
+++ frontend/src/app/search/search.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-search',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.css',
+})
+export class SearchComponent {
+  state: 'hidden' | 'loading' | 'results' = 'hidden';
+
+  handleSearch() {
+    this.state = 'loading';
+  }
+
+  closeSearch() {
+    this.state = 'hidden';
+  }
+}
