84f4704ed084b3d20c5c9bfb6a79a0507bef66c2
Munrhalls <antarcticdepths@gmail.com>
2024-06-22 19:14:59 +0200

    admin categs, solid solution

--- admin/src/app/pages/categories/categories.module.ts
+++ admin/src/app/pages/categories/categories.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ListComponent } from "./list/list.component";
 import { ItemComponent } from "./item/item.component";
-import { NbCardModule } from "@nebular/theme";
-
+import { NbCardModule, NbListModule, NbActionsModule } from "@nebular/theme";
 @NgModule({
   declarations: [ListComponent, ItemComponent],
-  imports: [CommonModule, NbCardModule],
-  exports: [ListComponent, ItemComponent, NbCardModule],
+  imports: [CommonModule, NbCardModule, NbListModule, NbActionsModule],
+  exports: [
+    ListComponent,
+    ItemComponent,
+    NbCardModule,
+    NbListModule,
+    NbActionsModule,
+  ],
 })
 export class CategoriesModule {}
--- admin/src/app/pages/categories/list/list.component.ts
+++ admin/src/app/pages/categories/list/list.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, ViewEncapsulation } from "@angular/core";
 
 @Component({
   selector: "ngx-list",
+  encapsulation: ViewEncapsulation.None,
   templateUrl: "./list.component.html",
   styleUrls: ["./list.component.scss"],
 })
