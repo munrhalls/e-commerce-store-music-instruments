d926a88a0dbbfd8202e681d48ea0f2e7fb36855d
Munrhalls <antarcticdepths@gmail.com>
2024-06-22 15:56:57 +0200

    awesome recursive solution for nested categories!

--- admin/src/app/pages/categories/categories.component.ts
+++ admin/src/app/pages/categories/categories.component.ts
@@ -1,19 +1,42 @@
 import { Component } from "@angular/core";
 import { ListComponent } from "./list/list.component";
 
+export interface Category {
+  id: number;
+  name: string;
+  children?: Category[];
+}
+
 @Component({
   selector: "ngx-categories",
   templateUrl: "./categories.component.html",
   styleUrls: ["./categories.component.scss"],
 })
 export class CategoriesComponent {
-  categories: any[] = [
+  categories: Category[] = [
     {
       id: 1,
       name: "Category 1",
       children: [
-        { id: 1.1, name: "Category 1.1" },
+        {
+          id: 1.1,
+          name: "Category 1.1",
+          children: [
+            { id: 1.11, name: "Category 1.1.1" },
+            { id: 1.12, name: "Category 1.1.2" },
+            {
+              id: 1.13,
+              name: "Category 1.1.3",
+              children: [
+                { id: 1.131, name: "Category 1.1.3.1" },
+                { id: 1.131, name: "Category 1.1.3.2" },
+                { id: 1.131, name: "Category 1.1.3.3" },
+              ],
+            },
+          ],
+        },
         { id: 1.2, name: "Category 1.2" },
+        { id: 1.2, name: "Category 1.3" },
       ],
     },
     { id: 2, name: "Category 2" },
--- admin/src/app/pages/categories/categories.module.ts
+++ admin/src/app/pages/categories/categories.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ListComponent } from "./list/list.component";
 import { ItemComponent } from "./item/item.component";
+import { NbCardModule } from "@nebular/theme";
 
 @NgModule({
   declarations: [ListComponent, ItemComponent],
-  imports: [CommonModule],
-  exports: [ListComponent, ItemComponent],
+  imports: [CommonModule, NbCardModule],
+  exports: [ListComponent, ItemComponent, NbCardModule],
 })
 export class CategoriesModule {}
