0beba26de48f511ad548f7233f455bb4248c1128
Munrhalls <antarcticdepths@gmail.com>
2024-06-25 16:12:42 +0200

    admin, categs on/off fit for anims

--- admin/src/app/pages/categories/item/item.component.ts
+++ admin/src/app/pages/categories/item/item.component.ts
@@ -7,4 +7,12 @@ import { Component, Input } from "@angular/core";
 })
 export class ItemComponent {
   @Input() category: any = { id: 0, name: "" };
+  isShowMenu = false;
+  isShowSubcategories = false;
+  toggleMenu() {
+    this.isShowMenu = !this.isShowMenu;
+  }
+  toggleSubcategories() {
+    this.isShowSubcategories = !this.isShowSubcategories;
+  }
 }
