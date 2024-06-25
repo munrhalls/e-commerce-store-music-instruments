a7047fcfefdb9dd382cbd47122a739166b9f177d
Munrhalls <antarcticdepths@gmail.com>
2024-06-25 15:11:09 +0200

    categories admin

--- admin/src/app/pages/categories/categories.module.ts
+++ admin/src/app/pages/categories/categories.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ListComponent } from "./list/list.component";
 import { ItemComponent } from "./item/item.component";
+import { MenuComponent } from "./menu/menu.component";
 
+import { NbIconModule } from "@nebular/theme";
 @NgModule({
-  declarations: [ListComponent, ItemComponent],
-  imports: [CommonModule],
-  exports: [ListComponent, ItemComponent],
+  declarations: [ListComponent, ItemComponent, MenuComponent],
+  imports: [CommonModule, NbIconModule],
+  exports: [ListComponent, ItemComponent, NbIconModule],
 })
 export class CategoriesModule {}
