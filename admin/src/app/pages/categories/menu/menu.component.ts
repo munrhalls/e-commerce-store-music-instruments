454c9fd5ad8ebdc132b2d6089398aa41da19e9e5
Munrhalls <antarcticdepths@gmail.com>
2024-06-27 19:15:09 +0200

    admin categories, reset form

--- admin/src/app/pages/categories/menu/menu.component.ts
+++ admin/src/app/pages/categories/menu/menu.component.ts
@@ -1,11 +1,4 @@
-import { Component, Input, TemplateRef } from "@angular/core";
-
-import {
-  FormBuilder,
-  FormGroup,
-  Validators,
-  AbstractControl,
-} from "@angular/forms";
+import { Component, Input, TemplateRef, OnInit } from "@angular/core";
 import { NbDialogService } from "@nebular/theme";
 
 @Component({
@@ -15,52 +8,7 @@ import { NbDialogService } from "@nebular/theme";
 })
 export class MenuComponent {
   @Input() category: any = { id: 0, name: "" };
-  subcategoryForm: FormGroup;
-
-  get nameControl(): AbstractControl | null {
-    console.log("status of the form name ctrl", () =>
-      this.subcategoryForm.get("name"),
-    );
-    return this.subcategoryForm.get("name");
-  }
-
-  constructor(
-    private dialogService: NbDialogService,
-    private fb: FormBuilder,
-  ) {
-    this.subcategoryForm = this.fb.group({
-      name: ["", Validators.required, Validators.maxLength(30)],
-    });
-  }
-
-  getSubcategoryControlStatus(controlName: string) {
-    console.log("form status", this.subcategoryForm.get(controlName).status);
-    return this.subcategoryForm.get(controlName).status;
-  }
-
-  onSubmit(ref: any, event: Event) {
-    console.log("submit!!!", this.subcategoryForm.value.name);
-    event.preventDefault();
-    console.log("now what?", this.subcategoryForm.valid);
-    if (this.subcategoryForm.valid) {
-      console.log("valid!");
-      const newSubcategory = {
-        id: Math.random(),
-        name: this.subcategoryForm.value.name,
-      };
-      this.addSubcategory(newSubcategory, this.category);
-      ref.close();
-    }
-  }
-
-  addSubcategory(newSubcategory: any, category: any) {
-    console.log("adding...");
-    if (!category.children) {
-      category.children = [];
-    }
-    category.children.push(newSubcategory);
-    console.log("added?...", category.children);
-  }
+  constructor(private dialogService: NbDialogService) {}
 
   open(dialog: TemplateRef<any>) {
     this.dialogService.open(dialog, {
