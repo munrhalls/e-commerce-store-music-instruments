6fd7e2cc9a1047e7588b58bbdfa66ecf66ebdbb0
Munrhalls <antarcticdepths@gmail.com>
2024-06-28 23:31:09 +0200

    admin categories, crud tests for DS

--- admin/src/app/pages/categories/categories.service.spec.ts
+++ admin/src/app/pages/categories/categories.service.spec.ts
@@ -1,17 +1,114 @@
 import { TestBed } from "@angular/core/testing";
-import { CategoriesService } from "./categories.service";
+import { CategoriesService, CategoryNode } from "./categories.service";
+import { cloneDeep } from "lodash";
 
 describe("CategoriesService", () => {
   let service: CategoriesService;
+  let categoryNode: CategoryNode;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [CategoriesService],
     });
     service = TestBed.inject(CategoriesService);
+    categoryNode = service.categoryNode;
   });
 
   it("should be created", () => {
     expect(service).toBeDefined();
+  });
+
+  it("Data Structure (DS) should have unique IDs across the entire structure", () => {
+    const checkForDuplicateIds = function (
+      node: CategoryNode,
+      allIds = new Set<string>(),
+    ): boolean {
+      if (allIds.has(node.id)) return true;
+      allIds.add(node.id);
+      for (const child of node.children) {
+        if (checkForDuplicateIds(child, allIds)) return true;
+      }
+      return false;
+    };
+
+    const hasDuplicates = checkForDuplicateIds(categoryNode);
+    expect(hasDuplicates).toBe(false);
+  });
+
+  it("DS should not have circular references", () => {
+    const checkForCircularReferences = function (
+      node: CategoryNode,
+      visitedNodes = new Set<string>(),
+    ): void {
+      if (visitedNodes.has(node.id)) {
+        throw new Error("Circular reference detected");
+      }
+
+      visitedNodes.add(node.id);
+
+      for (const child of node.children) {
+        checkForCircularReferences(child, visitedNodes);
+      }
+    };
+
+    expect(() => checkForCircularReferences(categoryNode)).not.toThrow();
+  });
+
+  it("DS should find a specific node by ID", () => {
+    const nodeId = "2.1";
+    const foundNode = service.findNodeById(categoryNode, nodeId);
+    expect(foundNode).toBeDefined();
+    expect(foundNode.id).toBe(nodeId);
+  });
+
+  it("should create a new node", () => {
+    const newCategory: CategoryNode = {
+      id: "3",
+      parentId: "root",
+      name: "Category 3",
+      children: [],
+    };
+    service.createNode("root", newCategory);
+    expect(service.findNodeById(categoryNode, "3")).toBeDefined();
+  });
+
+  it("should update an existing node", () => {
+    const updatedCategory: CategoryNode = {
+      id: "2.1",
+      parentId: "2",
+      name: "Updated Category 2.1",
+      children: [],
+    };
+    service.updateNode("2.1", updatedCategory);
+    const foundNode = service.findNodeById(categoryNode, "2.1");
+    expect(foundNode).toBeDefined();
+    expect(foundNode?.name).toBe("Updated Category 2.1");
+  });
+
+  it("delete - should throw an error when the parent node is not found", () => {
+    expect(() => service.deleteNode("nonexistentId", "2.1")).toThrowError(
+      "Parent node with ID nonexistentId not found",
+    );
+  });
+
+  it("should delete a child node of the root", () => {
+    service.deleteNode("root", "1");
+
+    expect(service.findNodeById(categoryNode, "1")).toBeUndefined();
+  });
+
+  it("should delete root when it has no children", () => {
+    const initialCategoryNode = cloneDeep(service.categoryNode);
+    initialCategoryNode.children = [];
+    service.categoryNode = initialCategoryNode;
+
+    service.deleteNode("root", initialCategoryNode.id);
+
+    expect(service.categoryNode).toEqual({
+      id: "root",
+      name: "",
+      parentId: "root",
+      children: [],
+    });
   });
 });
--- admin/src/app/pages/categories/categories.service.ts
+++ admin/src/app/pages/categories/categories.service.ts
@@ -5,16 +5,111 @@ import { map } from "rxjs/operators"; // Import map operator
 import { cloneDeep } from "lodash";
 // import { ObjectId } from 'bson';
 
-export interface Category {
-  id: number;
-  name: string;
-}
 export interface CategoryNode {
-  data: Category;
+  id: string;
+  parentId: string | "root";
+  name: string;
   children: CategoryNode[];
 }
 
 @Injectable({
   providedIn: "root",
 })
-export class CategoriesService {}
+export class CategoriesService {
+  categoryNode: CategoryNode = {
+    id: "root",
+    parentId: "root",
+    name: "Root",
+    children: [
+      {
+        id: "1",
+        parentId: "root",
+        name: "Category 1",
+        children: [],
+      },
+      {
+        id: "2",
+        parentId: "root",
+        name: "Category 2",
+        children: [
+          {
+            id: "2.1",
+            parentId: "2",
+            name: "Category 2.1",
+            children: [
+              {
+                id: "2.1.1",
+                parentId: "2.1",
+                name: "Category 2.1.1",
+                children: [],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  };
+  private categoryNodeSubject = new BehaviorSubject<CategoryNode>(
+    this.categoryNode,
+  );
+  categoryNode$ = this.categoryNodeSubject.asObservable();
+
+  findNodeById(node: CategoryNode, id: string): CategoryNode | undefined {
+    if (node.id === id) {
+      return node;
+    }
+    for (const child of node.children) {
+      const found = this.findNodeById(child, id);
+      if (found) {
+        return found;
+      }
+    }
+    return undefined;
+  }
+  createNode(parentId: string, newNode: CategoryNode): void {
+    const parent = this.findNodeById(this.categoryNode, parentId);
+
+    if (parent) {
+      parent.children.push(newNode);
+      this.categoryNodeSubject.next(cloneDeep(this.categoryNode));
+    } else {
+      throw new Error(`Parent node with ID ${parentId} not found`);
+    }
+  }
+  updateNode(nodeId: string, updatedNode: CategoryNode): void {
+    const nodeToUpdate = this.findNodeById(this.categoryNode, nodeId);
+    if (nodeToUpdate) {
+      Object.assign(nodeToUpdate, updatedNode);
+      this.categoryNodeSubject.next(cloneDeep(this.categoryNode));
+    } else {
+      throw new Error(`Node with ID ${nodeId} not found`);
+    }
+  }
+  deleteNode(parentId: string, childId: string): void {
+    if (parentId === "root" && childId === "root") {
+      this.categoryNode = {
+        id: "root",
+        name: "",
+        parentId: "root",
+        children: [],
+      };
+      return;
+    }
+    if (parentId === this.categoryNode.id) {
+      this.categoryNode.children = this.categoryNode.children.filter(
+        (child) => child.id !== childId,
+      );
+      this.categoryNodeSubject.next(cloneDeep(this.categoryNode));
+    } else {
+      const parentNode = this.findNodeById(this.categoryNode, parentId);
+      if (parentNode) {
+        parentNode.children = parentNode.children.filter(
+          (child) => child.id !== childId,
+        );
+        this.categoryNodeSubject.next(cloneDeep(this.categoryNode));
+      } else {
+        throw new Error(`Parent node with ID ${parentId} not found`);
+      }
+    }
+  }
+}
