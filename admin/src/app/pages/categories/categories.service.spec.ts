a4cbaea7fd19f2ec4a6111ea2d4184059e9e14fe
Munrhalls <antarcticdepths@gmail.com>
2024-06-30 11:03:46 +0200

    ADMIN CATEGORIES, service tests

--- admin/src/app/pages/categories/categories.service.spec.ts
+++ admin/src/app/pages/categories/categories.service.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from "@angular/core/testing";
-import { CategoriesService } from "./categories.service";
+import { CategoriesService, CategoryNode } from "./categories.service";
 
 describe("CategoriesService", () => {
   let service: CategoriesService;
@@ -15,5 +15,39 @@ describe("CategoriesService", () => {
     expect(service).toBeDefined();
   });
 
-  it("should output categories tree via recursive function", () => {});
+  it("should output each category via recursion", () => {
+    const categoryNode: CategoryNode = {
+      id: "root",
+      name: "root",
+      pathIds: ["root"],
+      children: [
+        {
+          id: "1",
+          name: "Category 1",
+          pathIds: ["root", "1"],
+          children: [],
+        },
+        {
+          id: "2",
+          name: "Category 2",
+          pathIds: ["root", "2"],
+          children: [
+            {
+              id: "2.1",
+              name: "Category 2.1",
+              pathIds: ["root", "2", "1"],
+              children: [
+                {
+                  id: "2.1.1",
+                  name: "Category 2.1.1",
+                  pathIds: ["root", "2", "1", "1"],
+                  children: [],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    };
+  });
 });
