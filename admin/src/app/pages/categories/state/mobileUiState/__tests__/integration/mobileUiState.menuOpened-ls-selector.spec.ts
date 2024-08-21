6b8f345001f5be1cd36ff2d70a84730d19e501f0
Munrhalls <antarcticdepths@gmail.com>
2024-08-21 19:13:17 +0200

    ADMIN PANEL / CATEGORIES - MOBILEUISTATE 2 - INTEGRATION TESTS DONE

--- admin/src/app/pages/categories/state/mobileUiState/__tests__/integration/mobileUiState.menuOpened-ls-selector.spec.ts
+++ admin/src/app/pages/categories/state/mobileUiState/__tests__/integration/mobileUiState.menuOpened-ls-selector.spec.ts
@@ -42,7 +42,7 @@ describe("INPUT ACTIONS -> OUTPUT SELECTOR STATE PROPER", () => {
     });
   });
 
-  it("action: allItemsUnfoldedToggle -> allItemsUnfoldedToggle: true", () => {
+  it("action: menuOpened -> menuOpened: id: 1", () => {
     setupTestBed("withLocalStorageSync");
 
     testScheduler.run(({ expectObservable }) => {
@@ -54,48 +54,7 @@ describe("INPUT ACTIONS -> OUTPUT SELECTOR STATE PROPER", () => {
         LS$.next(LSParsed.mobileUiState);
       });
 
-      store.dispatch(mobileUiStateActions.allItemsUnfoldedToggle());
-
-      expectObservable(LS$).toBe("(ab)", {
-        a: {
-          itemsUnfolded: {
-            all: false,
-            list: [],
-          },
-          menuOpened: {
-            id: null,
-            form: null,
-            deleteConfirmationId: null,
-          },
-        },
-        b: {
-          itemsUnfolded: {
-            all: true,
-            list: [],
-          },
-          menuOpened: {
-            id: null,
-            form: null,
-            deleteConfirmationId: null,
-          },
-        },
-      });
-    });
-  });
-
-  it("action: itemUnfolded -> mobileUiState.itemsUnfolded.list: [] -> list: [id]", () => {
-    setupTestBed("withLocalStorageSync");
-
-    testScheduler.run(({ expectObservable }) => {
-      const state$ = store.select(selectMobileUiState);
-      const LS$ = new ReplaySubject();
-      state$.subscribe(() => {
-        const LS = localStorage.getItem("categories");
-        const LSParsed = JSON.parse(LS);
-        LS$.next(LSParsed.mobileUiState);
-      });
-
-      store.dispatch(mobileUiStateActions.itemUnfolded({ id: "id" }));
+      store.dispatch(mobileUiStateActions.menuOpened({ id: "1" }));
 
       expectObservable(LS$).toBe("(ab)", {
         a: {
@@ -112,10 +71,10 @@ describe("INPUT ACTIONS -> OUTPUT SELECTOR STATE PROPER", () => {
         b: {
           itemsUnfolded: {
             all: false,
-            list: ["id"],
+            list: [],
           },
           menuOpened: {
-            id: null,
+            id: "1",
             form: null,
             deleteConfirmationId: null,
           },
@@ -124,8 +83,20 @@ describe("INPUT ACTIONS -> OUTPUT SELECTOR STATE PROPER", () => {
     });
   });
 
-  it("action: itemFolded -> mobileUiState.itemsUnfolded.list: [id] -> list: []", () => {
-    setupTestBed("withLocalStorageSync");
+  it("initial state: menuOpened 1 && deleteConfirmationId: 1  + action: menuOpened 2 -> state: menuOpened 2 && deleteConfirmationId: null", () => {
+    initialTestState = {
+      itemsUnfolded: {
+        all: false,
+        list: [],
+      },
+      menuOpened: {
+        id: "1",
+        form: null,
+        deleteConfirmationId: "1",
+      },
+    };
+
+    setupTestBed("withLocalStorageSync", initialTestState);
 
     testScheduler.run(({ expectObservable }) => {
       const state$ = store.select(selectMobileUiState);
@@ -136,39 +107,17 @@ describe("INPUT ACTIONS -> OUTPUT SELECTOR STATE PROPER", () => {
         LS$.next(LSParsed.mobileUiState);
       });
 
-      store.dispatch(mobileUiStateActions.itemUnfolded({ id: "1" }));
-      store.dispatch(mobileUiStateActions.itemFolded({ id: "1" }));
+      store.dispatch(mobileUiStateActions.menuOpened({ id: "2" }));
 
-      expectObservable(LS$).toBe("(abc)", {
-        a: {
-          itemsUnfolded: {
-            all: false,
-            list: [],
-          },
-          menuOpened: {
-            id: null,
-            form: null,
-            deleteConfirmationId: null,
-          },
-        },
+      expectObservable(LS$).toBe("(ab)", {
+        a: initialTestState,
         b: {
           itemsUnfolded: {
             all: false,
-            list: ["1"],
-          },
-          menuOpened: {
-            id: null,
-            form: null,
-            deleteConfirmationId: null,
-          },
-        },
-        c: {
-          itemsUnfolded: {
-            all: false,
             list: [],
           },
           menuOpened: {
-            id: null,
+            id: "2",
             form: null,
             deleteConfirmationId: null,
           },
